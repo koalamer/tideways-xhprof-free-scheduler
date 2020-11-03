dc7281d163557b61ad8f0a4bdbd4cc73da2e95b0
Balazs Fodor <fodorbalazs@freemail.hu>
2020-11-03 06:49:07 +0100

    Add description

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # tideways-xhprof-free-scheduler
+
 A small helper class to integrate Tideway's Xhprof free edition into your code.
+
+## How to Use
+
+### Use the class name in your script
+
+You can have the class autoloaded, but if you want to include the autoloading routine in the measurement, you should include the source file directly and start the profiling before including the autoloader.
+
+```php
+use koalamer\Tideways\TidewaysXhprofFreeScheduler;
+```
+
+### Set how often to start profiling
+
+Setting the value X will result in a 1 in X chance for the profiling to start. Setting it to 1 or smaller results in always starting the profiler.
+
+The default value is 10 000, which shouldn't be a low enough number to not cause atrouble if you forget to set your own value in production.
+
+```php
+TidewaysXhprofFreeScheduler::setStartingChanceDivisor(100);
+```
+
+### Start the actual profiling
+
+The init() call will use the starting chance to determine whether to actually start profiling or not. This call can be repeated as many times you want, each time the current starting chance divisor is used to evaluate whether to start profiling or not.
+This means, you can set a lower starting chance globally, and later try a higher starting chance for scripts you know start less often and therefore would otherwise have a low chance to be profiled.
+
+Once the profiling has started the call returns without doing anything.
+
+If the tideways_xhprof extension is not loaded, the call returns false, otherwise true.
+
+```php
+TidewaysXhprofFreeScheduler::init()
+```
+
+### Set the log file path by defining segmentation dimensions
+
+You can set the log file path by defining segmentation. Each level of segmentation corresponds to a directory level. The key-value pairs you provide are only evaluated when the profiling stops, so you can define and redefine segmentation levels throughout your code.
+
+The profiling stops when the script shuts down. The collection of segmentation parameters is then sorted by key and imploded, so the keys you use matter.
+
+If you provide an empty string as the value, that value will be replaced by the string "empty".
+
+For example, defining these segmentations:
+
+```php
+TidewaysXhprofFreeScheduler::setSegmentation("1-host", gethostname())
+TidewaysXhprofFreeScheduler::setSegmentation("3-module", "awesome-module")
+TidewaysXhprofFreeScheduler::setSegmentation("2-day", date("Y-m-d"))
+TidewaysXhprofFreeScheduler::setSegmentation("0-root","/tmp")
+TidewaysXhprofFreeScheduler::setSegmentation("4-action","")
+```
+
+will result in a log file path "/tmp/some-hostname/2020-11-03/awesome-module/empty/".
+
+The log file name itself will be the current time in the form of date, time and microsecs like this: "20201103_064423_032472.json".
--- src/TidewaysXhprofFreeScheduler.php
+++ src/TidewaysXhprofFreeScheduler.php
@@ -25,13 +25,24 @@ SOFTWARE.
 
 namespace koalamer\Tideways;
 
-class TidewaysXhprofFreeScheduler {
+class TidewaysXhprofFreeScheduler
+{
 
 	private static $singleton;
 	private static $startingChanceDivisor = 10000;
 	private static $segmentation = [];
 
-	public static function init(): bool{
+	/**
+	 * Starts the profiling. If the starting chance divisor is 1 or smaller, the
+	 * profiling is always started. If this value is above 1, let's say X, then
+	 * the profiling is started with a 1 in X chance randomly.
+	 * If the extension is not loaded, the call returns false, otherwise true.
+	 * Multiple calls to init() are allowed, eacj=h call will use the current
+	 * starting chance. If an earlyer call already started the profiling, the
+	 * call returns without doing anything.
+	 */
+	public static function init(): bool
+	{
 		if (!extension_loaded('tideways_xhprof')) {
 			return false;
 		}
@@ -43,9 +54,9 @@ class TidewaysXhprofFreeScheduler {
 		if (self::$startingChanceDivisor > 1) {
 			srand((int)(microtime(true) * 1000));
 			$luckyNumber = rand(1, self::$startingChanceDivisor);
-			if ($luckyNumber !== 1){
+			if ($luckyNumber !== 1) {
 				return true;
-			}	
+			}
 		}
 
 		tideways_xhprof_enable(TIDEWAYS_XHPROF_FLAGS_CPU | TIDEWAYS_XHPROF_FLAGS_MEMORY);
@@ -53,12 +64,31 @@ class TidewaysXhprofFreeScheduler {
 		return true;
 	}
 
-	public static function setStartingChanceDivisor(int $divisor){
+	/**
+	 * Sets the likelyhood ratio with which to start the profiling when init()
+	 * is called. Setting the value X will result in a 1 in X chance to start,
+	 * unless X is 1 or smaller, in which case the profiling is always started.
+	 * If you don't set a value, the default of 10 000 is used.
+	 */
+	public static function setStartingChanceDivisor(int $divisor)
+	{
 		self::$startingChanceDivisor = $divisor;
 	}
 
-	public static function setSegmentation(string $key, string $value){
-		if( strlen($value) ){
+	/**
+	 * Sets one level of segmentation for the resulting log file in the form of
+	 * defining one directory level of the logging path.
+	 * There is no default logging path, so you should set call this function at
+	 * least once to define the path where the log files should be put.
+	 * The log file path is assembled when the profiling is stopped, so you can
+	 * call this function as many times you want, even after init() was fired.
+	 * If an empty value is provided, it will be replaced by the string "empty".
+	 * When assembling the final log file path, the collection of segmentation
+	 * levels is sorted by key and imploded.
+	 */
+	public static function setSegmentation(string $key, string $value)
+	{
+		if (strlen($value)) {
 			self::$segmentation[$key] = $value;
 			return;
 		}
@@ -66,15 +96,16 @@ class TidewaysXhprofFreeScheduler {
 		self::$segmentation[$key] = 'empty';
 	}
 
-	function __destruct() {
+	function __destruct()
+	{
 		$logPath = $this->getLogPath();
 
-		if (!file_exists($logPath)){
+		if (!file_exists($logPath)) {
 			mkdir($logPath, 0777, true);
 		}
-		
+
 		if (!is_dir($logPath)) {
-			return;	
+			return;
 		}
 
 		$time = explode(' ', microtime(false));
@@ -83,7 +114,7 @@ class TidewaysXhprofFreeScheduler {
 			$logPath,
 			DIRECTORY_SEPARATOR,
 			date('Ymd_His_', $time[1]),
-			(int)($time[0]*1000000),
+			(int)($time[0] * 1000000),
 			'.json'
 		);
 		$data = tideways_xhprof_disable();
@@ -91,8 +122,13 @@ class TidewaysXhprofFreeScheduler {
 		file_put_contents($logFileName, json_encode($data));
 	}
 
-	private function getLogPath() : string {
+	private function getLogPath(): string
+	{
 		$segmentation = self::$segmentation;
+		if (!count($segmentation)) {
+			return '.';
+		}
+
 		ksort($segmentation);
 		return implode(DIRECTORY_SEPARATOR, $segmentation);
 	}
